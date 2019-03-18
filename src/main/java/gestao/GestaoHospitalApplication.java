d746cd33232147459afd52a1a690b5eab3a8ef5d
Leonardo Simões <leosimoes.engcomp@gmail.com>
2019-03-18 10:32:24 -0300

    Inicio

---
+++ src/main/java/gestao/GestaoHospitalApplication.java
@@ -0,0 +1,13 @@
+package gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoHospitalApplication.class, args);
+	}
+
+}
